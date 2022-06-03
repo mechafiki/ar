3485387a58f4bd8190957c557524224ce92c25c9
Mohamed El Amine Chafiki <mohamedelamine.chafiki@edu.uiz.ac.ma>
2022-06-03 17:29:47 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -14,6 +14,6 @@ firebase.initializeApp(firebaseConfig);
 var markers = firebase.storage().ref("marker")
 var objects = firebase.storage().ref("obj")
 console.log(objects)
-listAll(markers)
+listAll(objects)
 
 
